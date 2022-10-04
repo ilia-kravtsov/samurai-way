b1e24934068a7de12adbb1dd11bb958013e349a7
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-10-04 13:58:55 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello, Viking! Let's go!</h1>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
