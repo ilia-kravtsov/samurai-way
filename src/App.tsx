595e75a6a3155ee8a014d199a231cb61d5df2c54
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-10-04 21:27:16 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Technologies from './Technologies';
 
 const App = () => {
     return (
@@ -7,30 +9,6 @@ const App = () => {
             <Header/>
             <Technologies/>
         </div>
-    );
-}
-
-const Header = () => {
-    return (
-        <div>
-            <a href="#">Home</a>
-            <a href="#">News Feed</a>
-            <a href="#">Messages</a>
-        </div>
-    );
-}
-
-const Technologies = () => {
-    return (
-        <div>
-            <ul>
-                <li>html</li>
-                <li>css</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-
     );
 }
 
