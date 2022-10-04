01e82fff6f20c5b7d524c748517d43a371892758
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-10-04 21:27:26 +0300

    Create Header.tsx

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import App from "./App";
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    );
+}
+
+export default Header;
