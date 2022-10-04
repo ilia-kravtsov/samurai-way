868ef1d6254197fc1856438911b50ad8c54d59ed
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-10-04 21:27:35 +0300

    Create Technologies.tsx

---
+++ src/Technologies.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import App from "./App";
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+
+    );
+}
+
+export default Technologies;
