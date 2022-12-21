e28001382c7b2f2a3e94bf299a43379c77b9cfeb
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-12-21 14:26:46 +0400

    Update Header.tsx

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.headers}>
-            <img src='https://pluspng.com/img-png/avengers-logo-png-transparent-aou-logo-png-844.png' alt='groot'/>
+            <img src='https://pluspng.com/img-png/avengers-logo-png-transparent-aou-logo-png-844.png' alt='avengers logo'/>
         </header>
     );
 }
